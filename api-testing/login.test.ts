69b6c2084637ad4f7d90d077149311396ef874c6
Denys Pavlyuk <densf2@gmail.com>
2025-05-17 12:48:47 +0300

    used-interface-for-getting-token

--- api-testing/login.test.ts
+++ api-testing/login.test.ts
@@ -3,6 +3,13 @@ import jsonpath from "jsonpath";
 import fs from "fs-extra";
 import jsonData from "../api-data.json";
 
+interface JsonData {
+  baseUrl: string;
+  token?: string;
+}
+// Cast jsonData to the interface
+const typedJsonData = jsonData as JsonData;
+
 let userName: string;
 let userPass: string;
 
@@ -10,13 +17,13 @@ describe("authorization", () => {
   test("get all users", async () => {
     const all_users_response = await axios.get(`${jsonData.baseUrl}/users`);
     userName = String(
-      jsonpath.query(all_users_response.data, "$..users[3].username"),
+      jsonpath.query(all_users_response.data, "$..users[3].username")
     );
     const userName12 = String(
-      jsonpath.query(all_users_response.data, "$..users[?(@.id==12)].username"),
+      jsonpath.query(all_users_response.data, "$..users[?(@.id==12)].username")
     );
     userPass = String(
-      jsonpath.query(all_users_response.data, "$..users[3].password"),
+      jsonpath.query(all_users_response.data, "$..users[3].password")
     );
     console.log(userName + " pass: " + userPass);
     console.log(userName12);
@@ -34,9 +41,9 @@ describe("authorization", () => {
         headers: {
           "Content-Type": "application/json",
         },
-      },
+      }
     );
-    jsonData.token = auth_token_response.data.token;
-    fs.writeJSONSync("api-data.json", jsonData);
+    typedJsonData.token = auth_token_response.data.token;
+    fs.writeJSONSync("api-data.json", typedJsonData);
   });
 });
