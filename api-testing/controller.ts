e73f44c1c47aec7ed47c5c2dae1164cca220dc52
Denys Pavlyuk <densf2@gmail.com>
2026-02-25 23:13:54 +0200

    defined-data-type

--- api-testing/controller.ts
+++ api-testing/controller.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export class ApiControllers {
-  async getUserById(userId: any) {
-    let r = await axios.get(`https://dummyjson.com/users/${userId}`);
+  async getUserById(userId: string | number) {
+    const r = await axios.get(`https://dummyjson.com/users/${userId}`);
     expect(r.status).toBe(200);
   }
 }
