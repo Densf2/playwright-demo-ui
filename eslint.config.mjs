d09971c79758786c8640c97620830dc6389040a2
Denys Pavlyuk <densf2@gmail.com>
2026-08-12 21:42:30 +0300

    added-ignore-files-for-jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,15 @@ import tseslint from "typescript-eslint";
 
 
 export default [
+  {
+    ignores: [
+      "jest-html-reporters-attach/",
+      "playwright-report/",
+      "test-results/",
+      "blob-report/",
+      "playwright/.cache/",
+    ],
+  },
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
